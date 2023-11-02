a8cd29c9636f581d8b45f7019c9ce61af822bef8
Dhruv <dhruvanandsingh2004@gmail.com>
2023-11-02 11:08:09 +0530

    Added Sections folder and site design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import Hero from './sections/Hero'
+import CustomerReviews from './sections/CustomerReviews'
+import PopularProducts from './sections/PopularProducts'
+import Services from './sections/Services'
+import Subscribe from './sections/Subscribe'
+import Footer from './sections/Footer'
+
+const App = () => (
+  <main className="relative">Navbar 
+  <section className="xl:padding-1 wide:padding-r padding-b">
+    Hero
+  </section>
+  <section className="padding">
+    Popular Products
+  </section>
+  <section className="padding">
+    Super Quality
+  </section>
+  <section className="padding-x py-10">
+    Services
+  </section>
+  <section className="padding">
+    Special Offer
+  </section>
+  <section className="padding bg-pale-blue">
+    Customer Reviews
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full">
+    Subscribe
+  </section>
+  <section className="padding">
+    
+  </section>
+  </main>
+)
+
+export default App;
