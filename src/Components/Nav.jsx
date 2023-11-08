93d405858af914d10f452a1b29ecbeb6e6a0e172
Dhruv <dhruvanandsingh2004@gmail.com>
2023-11-09 01:23:46 +0530

    Done

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {headerLogo} from '../assets/images';
-import {hamburger} from '../assets/icons';
 import {navLinks} from '../constants'
+import { hamburger } from '../assets/icons';
 const Nav = () => {
+
   return (
+    
  <header className='padding-x py-8 absolute z-10 w-full'>
     <nav className='flex items-center justify-between  '>
         <a href="/">
             <img src={headerLogo} alt="LOGO" width={130} height={30}/>
         </a>
         <ul className='flex-1 flex justify-center items gap-16 max-lg:hidden '>
-            {navLinks.map((item) =>(
-                <li key={item.label}>
-                    <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray hover:text-coral-red'>
-                        {item.label}
-                    </a>
-                </li>
-            ))}
-        </ul>
-        <div className='hidden max-lg:block'><img src={hamburger} alt="Hamburger" width={25} height={25}/></div>
-        
-    </nav>
+{navLinks.map((item) =>(
+    <li key={item.label}>
+        <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray hover:text-coral-red'>
+            {item.label}
+        </a>
+    </li>
+))}
+</ul>
+<div className='hidden max-lg:block'><img src={hamburger} alt="Hamburger" width={25} height={25}/></div>
+
+
+            </nav>
  </header>
   )
 }
 
 export default Nav
-Nav
+
